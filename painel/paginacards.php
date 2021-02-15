3ffb2d7a4938520343f934472885a7e99c421e02
laura <laura@LAPTOP-5OQ997SP>
2021-02-15 20:42:19 -0300

    Iniciar edição de bory

--- painel/paginacards.php
+++ painel/paginacards.php
@@ -1,22 +1,19 @@
 
-<!DOCTYPE HTML>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Projeto Final</title>
-    
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!--Bootstrap-->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
-    <!--JS-->
-    <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js" />
-    
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Projeto Final</title>
 
- <body>
-        <?php
-        // put your code here
-        ?>
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+        <!--JS-->
+        <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js" />
+
+    </head>
+
+    <body>
+<hi>Hi, world</hi>
     </body>
 </html>
