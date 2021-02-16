03000154a5102f7b0ac4b97e1c50c21d63082ddc
User <User@DESKTOP-4FVV4N6>
2021-02-16 01:53:07 -0200

    Insercao contato e 404

--- site/paginas/includes/footer.php
+++ site/paginas/includes/footer.php
@@ -1,8 +1,25 @@
-<?php
+ <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.1.0-rc
+    </div>
+    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
 
+<!-- jQuery -->
+<script src="painel/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="painel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="painel/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="painel/dist/js/demo.js"></script>
+</body>
+</html>
--- site/paginas/includes/header.php
+++ site/paginas/includes/header.php
@@ -1,8 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>AdminLTE 3 | 404 Page not found</title>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="painel/plugins/fontawesome-free/css/all.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
+    </head>
+    <body class="hold-transition sidebar-mini">
+        <div class="wrapper">
 
