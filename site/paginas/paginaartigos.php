d57f8e8b9e13096b5e6348ab55747da8258e03b4
Sesse Cavalcanti <72923307+SemiramisCavalcanti@users.noreply.github.com>
2021-02-22 11:39:22 -0300

    linkagem do footer e introdução do header e footer

--- site/paginas/paginaartigos.php
+++ site/paginas/paginaartigos.php
@@ -1,78 +1,41 @@
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>FinalPWmanha | Página de artigos</title>
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="painel/plugins/fontawesome-free/css/all.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
+<div class="container">
+    <div class="row">
+        <img src="../../painel/assets/img/logosenac" class="img-fluid m-auto w-25" alt="Imagem responsiva" >        
+    </div>
+    <div class="row m-auto text-center w-25">
+        <small class="text-justify">Study Room Co-Working</small>
+    </div>
+    <div class="row">
+        <div class="alert alert-primary m-5">
+            Área de conhecimento
+        </div>
+    </div>
 
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="painel/plugins/fontawesome-free/css/all.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
-    </head>
-    <body>
+    <div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">Título do Artigo</div>
+        <div class="card-body text-primary">
+            <small>Nome da (o) autora:</small> <br />
+            <small>Data de postagem:</small> <br />
+            <small>E-mail para contato</small>
+        </div>
 
-        <div class="container">
-            <div class="row">
-                <img src="../../painel/assets/img/logosenac" class="img-fluid m-auto w-25" alt="Imagem responsiva" >        
-            </div>
-            <div class="row m-auto text-center w-25">
-                <small class="text-justify">Study Room Co-Working</small>
-            </div>
-            <div class="row">
-                <div class="alert alert-primary m-5">
-                    Área de conhecimento
+        <div class="row">
+            <div class="card text-center">
+                <div class="card-header">
+                    Destaque
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Título especial</h5>
+                    <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+                    <a href="#" class="btn btn-primary">Visitar</a>
+                </div>
+                <div class="card-footer text-muted">
+                    2 dias atrás
                 </div>
             </div>
 
-            <div class="card border-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Título do Artigo</div>
-                <div class="card-body text-primary">
-                    <small>Nome da (o) autora:</small> <br />
-                    <small>Data de postagem:</small> <br />
-                    <small>E-mail para contato</small>
-                </div>
+        </div>
 
-                <div class="row">
-                    <div class="card text-center">
-                        <div class="card-header">
-                            Destaque
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Título especial</h5>
-                            <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
-                            <a href="#" class="btn btn-primary">Visitar</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            2 dias atrás
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-
-
-
-
-            <script src="painel/plugins/jquery/jquery.min.js"></script>
-            <!-- Bootstrap 4 -->
-            <script src="painel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-            <!-- AdminLTE App -->
-            <script src="painel/dist/js/adminlte.min.js"></script>
-            <!-- AdminLTE for demo purposes -->
-            <script src="painel/dist/js/demo.js"></script><!-- /.control-sidebar -->
-    </body>
-</html>
+    </div>
