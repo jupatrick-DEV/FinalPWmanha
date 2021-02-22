af07a9cd8c04ae1135a7169d00c547d24684ac80
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-IH8VEDN>
2021-02-22 10:52:55 -0300

    inclusão do header, footer e menus.

--- site/paginas/paginaartigos.php
+++ site/paginas/paginaartigos.php
@@ -14,12 +14,6 @@
         <!-- Theme style -->
         <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
 
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="painel/plugins/fontawesome-free/css/all.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="painel/dist/css/adminlte.min.css">
     </head>
     <body>
 
