bc4266c6aa00c55665dcd82a63953826bbf5ac11
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-K37320V>
2021-02-24 10:04:51 -0300

    criação do arquivo do painel adm OK criação da pagina login OK

---
+++ painel.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once 'painel/bd/conexao.php';
+include_once 'painel/helper/funcoes.php';
+
+
+$pg = isset($_GET['pg']);
+
+
+if ($pg) {
+
+
+    switch ($_GET['pg']) {
+
+
+        case 'login':
+            include_once 'painel/paginas/acesso/login.php';
+            break;
+
+        case 'dashboard':
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/dashboard.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+
+        default:
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/dashboard.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+    }
+} else {
+//não existe   
+    include_once 'site/paginas/includes/header.php';
+    include_once 'site/paginas/includes/menus.php';
+    include_once 'site/paginas/includes/erro.php';
+    include_once 'site/paginas/includes/footer.php';
+}
+
