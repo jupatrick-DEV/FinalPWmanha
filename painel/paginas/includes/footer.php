cf883e7f878552de7621b12e50550e792f45c1db
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-K37320V>
2021-02-24 12:25:21 -0300

    tentativa de valida~]ao de artigo atualização no painel, footer, menu e header

--- painel/paginas/includes/footer.php
+++ painel/paginas/includes/footer.php
@@ -2,20 +2,20 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.1.0-rc
     </div>
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2014-2020 AdminLTE.io</a>.</strong> All rights reserved.
   </footer>
 
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="painel/plugins/jquery/jquery.min.js"></script>
+<script src="../../../painel/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="painel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../../painel/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="painel/dist/js/adminlte.min.js"></script>
+<script src="../../../painel/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="painel/dist/js/demo.js"></script>
+<script src="../../../painel/dist/js/demo.js"></script>
 </body>
 </html>
 
