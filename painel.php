af284bf91a95b9e56489138ded4cd82b3b54d774
Sesse Cavalcanti <72923307+SemiramisCavalcanti@users.noreply.github.com>
2021-02-25 09:55:15 -0300

    atualização

--- painel.php
+++ painel.php
@@ -43,7 +43,7 @@ if ($pg) {
        
         case 'validar-artigo':
             $resultDados = new conexao ();
-            $dados = $resultDados->selecionaDados('SELECT * FROM facavoce mesmo');
+            $dados = $resultDados->selecionaDados('SELECT * FROM facavocemesmo');
             include_once 'site/paginas/includes/header.php';
 
             include_once 'painel/paginas/pagina-validar-artigo.php';
